7dff8fa52f2b49899caf74e4f711ceca6d96ecdd
Jamal Amar Bahena <jamal@Jamals-MacBook-Pro.local>
2019-01-15 16:09:39 -0600

    tip calculator express application created

--- server.js
+++ server.js
@@ -3,14 +3,12 @@ let express = require('express')
 let app = express()
 
 
-app.get('/greeting/:name/', (req, res) => {
-  res.send(`Hello/${req.params.name} it's great to see you!`)
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+		let total = `.${req.params.tipPercentage}` * `${req.params.total}`
+ 		 res.send(total)
 });
 
 
-// app.get('/', (req, res) => {
-// 	res.send('hello world')
-// })
 
 app.listen(9000)
 
